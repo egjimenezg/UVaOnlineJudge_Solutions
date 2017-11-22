e31f3addca16260b7687b35f36252dedb692e298
egjimenezg <egjimenezg@gmail.com>
2017-11-22 00:34:01 -0600

    Adding README.md

--- 10895_MatrixTranspose/MatrixTranspose.cpp
+++ 10895_MatrixTranspose/MatrixTranspose.cpp
@@ -13,83 +13,82 @@ AdjacencyList getTranspose(int,AdjacencyList*);
 void printTranspose(int,int,AdjacencyList*);
 
 int main(){
-	int rows,cols;
-	int i,j;
-	while(scanf("%d %d",&rows,&cols) != EOF){
-	  AdjacencyList graph = createGraph(rows);
-	  AdjacencyList transpose =  getTranspose(cols,&graph);
-	  printTranspose(cols,rows,&transpose);
+  int rows,cols;
+  int i,j;
+  while(scanf("%d %d",&rows,&cols) != EOF){
+    AdjacencyList graph = createGraph(rows);
+    AdjacencyList transpose =  getTranspose(cols,&graph);
+    printTranspose(cols,rows,&transpose);
   }
   return 0;
 }
 
 AdjacencyList createGraph(int rows){
-	AdjacencyList graph(rows);
-	int r,i,columnIndex,rowIndex,columnValue;
+  AdjacencyList graph(rows);
+  int r,i,columnIndex,rowIndex,columnValue;
 
-	for(rowIndex=0;rowIndex<rows;rowIndex++){
-		scanf("\n%d",&r);
+  for(rowIndex=0;rowIndex<rows;rowIndex++){
+    scanf("\n%d",&r);
 
-		for(i=0;i<r;i++){
-			scanf("%d ",&columnIndex);
-			graph[rowIndex].push_back(make_pair(columnIndex,0));
-		}
+    for(i=0;i<r;i++){
+      scanf("%d ",&columnIndex);
+      graph[rowIndex].push_back(make_pair(columnIndex,0));
+    }
 
-		for(i=0;i<r;i++){
-			scanf("%d ",&columnValue);
-			graph[rowIndex][i].second = columnValue;
-		}
+    for(i=0;i<r;i++){
+      scanf("%d ",&columnValue);
+      graph[rowIndex][i].second = columnValue;
+    }
 
-	}
+  }
 
-	return graph;
+  return graph;
 }
 
 AdjacencyList getTranspose(int rows,AdjacencyList* adjacencyList){
-	AdjacencyList transpose;
-	int rowIndex,colIndex;
+  AdjacencyList transpose;
+  int rowIndex,colIndex;
 
-	if(adjacencyList->size()){
-		transpose.resize(rows);
+  if(adjacencyList->size()){
+    transpose.resize(rows);
 
-		for(rowIndex=0;rowIndex<rows;rowIndex++){
-			for(colIndex=0;colIndex<adjacencyList->size();colIndex++){
-				if(adjacencyList->at(colIndex).size()){
-					Neighbor neighbor = adjacencyList->at(colIndex).front();
+    for(rowIndex=0;rowIndex<rows;rowIndex++){
+      for(colIndex=0;colIndex<adjacencyList->size();colIndex++){
+        if(adjacencyList->at(colIndex).size()){
+          Neighbor neighbor = adjacencyList->at(colIndex).front();
 
-					if(neighbor.first == rowIndex+1){
-						neighbor.first = colIndex+1;
-						transpose[rowIndex].push_back(neighbor);
-						adjacencyList->at(colIndex).pop_front();
-					}
-				}
-			}
-		}
+          if(neighbor.first == rowIndex+1){
+            neighbor.first = colIndex+1;
+            transpose[rowIndex].push_back(neighbor);
+            adjacencyList->at(colIndex).pop_front();
+          }
+        }
+      }
+    }
+  }
 
-	}
-
-	return transpose;		
+  return transpose;
 }
 
 void printTranspose(int rows,int cols,AdjacencyList* transpose){
-	printf("%d %d\n",rows,cols);
-	int rowsIndex,columnsIndex;
+  printf("%d %d\n",rows,cols);
+  int rowsIndex,columnsIndex;
 
-	for(rowsIndex=0;rowsIndex<rows;rowsIndex++){
-		if(transpose->size()){
-			printf("%lu",transpose->at(rowsIndex).size());
-			for(columnsIndex=0;columnsIndex<transpose->at(rowsIndex).size();columnsIndex++){
-				printf(" %d",transpose->at(rowsIndex)[columnsIndex].first);
-			}
-			printf("\n");
-			for(columnsIndex=0;columnsIndex<transpose->at(rowsIndex).size();columnsIndex++){
-				printf("%s%d",columnsIndex > 0 ? " " : "",transpose->at(rowsIndex)[columnsIndex].second);
-			}
-			printf("\n");
-		}
-		else{
-			printf("0\n\n");
-		}
-	}
+  for(rowsIndex=0;rowsIndex<rows;rowsIndex++){
+    if(transpose->size()){
+      printf("%lu",transpose->at(rowsIndex).size());
+      for(columnsIndex=0;columnsIndex<transpose->at(rowsIndex).size();columnsIndex++){
+        printf(" %d",transpose->at(rowsIndex)[columnsIndex].first);
+      }
+      printf("\n");
+      for(columnsIndex=0;columnsIndex<transpose->at(rowsIndex).size();columnsIndex++){
+        printf("%s%d",columnsIndex > 0 ? " " : "",transpose->at(rowsIndex)[columnsIndex].second);
+      }
+      printf("\n");
+    }
+    else{
+      printf("0\n\n");
+    }
+  }
 
 }
---
+++ 10895_MatrixTranspose/README.md
@@ -0,0 +1,4 @@
+# Problem
+
+[Matrix Transpose](https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1836)
+
